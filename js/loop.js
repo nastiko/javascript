cb0a79cefe65af5b9181740e922aed150f0f7c1e
Anastasia <anastasia.hrynkevich@outlook.com>
2022-09-23 19:24:48 +0100

    Worked on while, for in, for of. Did tasks from JS book

--- js/loop.js
+++ js/loop.js
@@ -37,26 +37,158 @@ for(let elem of arr) {
 
 //--------------
 
-let obj = {x:1, y:2, z:3};
+/*let obj = {x:1, y:2, z:3};
 
 for(let key in obj) {
-    //console.log(obj[key]);
-    console.log(key);
-}
+    console.log(obj[key]);
+    //console.log(key);
+}*/
 
 //--------------
+
+/*let i = 0
+
+while(i <= 99) {
+    i++;
+    console.log(i);
+}*/
+
 //--------------
+
+/*let i = 10;
+
+while(i <=32) {
+    i ++;
+    console.log(i);
+}*/
+
 //--------------
+
+/*let num = 30;
+
+while(num < 1000) {
+    num = num * 3;
+    console.log(num);
+}
+
+console.log(num);*/
+
 //--------------
+
+/*let num = 500;
+
+while (num > 10) {
+    num = num / 2;
+}
+
+console.log(num);*/
+
 //--------------
+
+/*let arr = [1, 2, 3, 4, 5];
+
+for(let i = 0; i <= arr.length; i++) {
+    console.log(arr[i]);
+}*/
+
 //--------------
+
+//let arr = ['a', 'b', 'c', 'd', 'e'];
+
+/*for(let i = 0; i <= arr.length; i++) {
+    console.log(arr[i]);
+}*/
+
+/*for (let i = 1; i <= 3; i++) {
+    console.log(arr[i]);
+}*/
+
+/*for(let i = arr.length; i >= 0; i--) {
+    console.log(arr[i]);
+}*/
+
+/*for (let i = 0; i <= arr.length -1; i++) {
+    console.log(arr[i]);
+}*/
+
 //--------------
+
+/*let arr = [1, 2, 3, 4, 5];
+
+for(let elem of arr) {
+    if(elem % 2 === 0){
+        console.log(elem);
+    }
+}*/
+
 //--------------
+
+/*let arr = [2, 5, 9, 15, 1, 4];
+
+for(let elem of arr) {
+    if(elem > 3 && elem < 10) {
+        console.log(elem);
+    }
+}*/
+
 //--------------
+
+/*let obj = {a:1, b:2, c:3, d:4, e:5};
+
+for(let key in obj) {
+    if(obj[key] % 2 !== 0) {
+        console.log(key);
+        console.log(obj[key]);
+    }
+}*/
+
 //--------------
+
+/*let res = 0;
+
+for (let i = 1; i <= 100; i++) {
+    res = res + i;
+    console.log(res);
+}
+
+console.log(res);*/
+
 //--------------
+
+/*let res = 0;
+
+for(let i = 2; i <= 100; i++) {
+    res = res + i;
+    if(res % 2 === 0) {
+
+        console.log(res);
+    }
+}
+
+console.log(res);*/
+
 //--------------
+
+/*let num = 0;
+
+for(let i = 1; i <= 99; i++) {
+    num += i;
+    if(num % 2 !== 0) {
+        console.log(num);
+    }
+}
+
+console.log(num);*/
+
 //--------------
+
+let num = 1;
+
+for(let i = 1; i <= 5; i++) {
+    num *= i;
+    console.log(num);
+}
+
 //--------------
 //--------------
 //--------------
