208f054af5f614631ad6857799d54e08e3af6bba
Anastasia <anastasia.hrynkevich@outlook.com>
2022-11-03 19:42:20 +0000

    Started to learn destructuring in JS

---
+++ js/destructuring.js
@@ -0,0 +1,129 @@
+function destructuringPractice1() {
+    let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+    let [firstName, secondName, course, profession, year] = arr;
+
+    console.log(course);
+}
+
+//destructuringPractice1();
+
+//--------------
+
+function destructuringPractice2() {
+    function func() {
+        return ['John', 'Smit', 'development', 'programmer', 2000];
+    }
+
+    let [firstName, secondName, course, profession, year] = func();
+    console.log(secondName);
+}
+
+//destructuringPractice2();
+
+//--------------
+
+function destructuringPractice3() {
+    let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+    let [,, course, profession,] = arr;
+
+    console.log(course, profession);
+}
+
+//destructuringPractice3();
+
+//--------------
+
+function destructuringPractice4() {
+    let arr = [2025, 12, 31, 23, 59, 59];
+    let [year, month, day] = arr;
+
+    console.log(year);
+    console.log(month);
+    console.log(day);
+}
+
+//destructuringPractice4();
+
+//--------------
+
+function destructuringPractice5() {
+
+}
+
+//destructuringPractice1();
+
+//--------------
+
+function destructuringPractice6() {
+
+}
+
+//destructuringPractice1();
+
+//--------------
+
+function destructuringPractice7() {
+
+}
+
+//destructuringPractice1();
+
+//--------------
+
+function destructuringPractice8() {
+
+}
+
+//destructuringPractice1();
+
+//--------------
+
+function destructuringPractice9() {
+
+}
+
+//destructuringPractice1();
+
+//--------------
+
+function destructuringPractice10() {
+
+}
+
+//destructuringPractice1();
+
+//--------------
+
+function destructuringPractice11() {
+
+}
+
+//destructuringPractice1();
+
+//--------------
+
+function destructuringPractice12() {
+
+}
+
+//destructuringPractice1();
+
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
+//--------------
