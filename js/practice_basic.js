379b579a57d2f678516c5783c5b79b7fd07a5d92
Anastasia <anastasia.hrynkevich@outlook.com>
2023-01-13 18:23:20 +0000

    docs: - read some articles for better understanding topics which I learnt before init: - start to consolidate my knowledge

---
+++ js/practice_basic.js
@@ -0,0 +1,113 @@
+//PRACTICE #1
+function createElemArray() {
+    let block = document.getElementById('basic1-block');
+    let arr = [1, 2, 3, 4, 5];
+
+    for(let i = 0; i < arr.length; i++) {
+        let paragraphs = document.createElement('p');
+        paragraphs.textContent = ` Number: ${arr[i]}`;
+
+        paragraphs.addEventListener('click', function() {
+            paragraphs.textContent = ` Number: ${++arr[i]}`;
+        });
+
+        block.appendChild(paragraphs);
+    }
+}
+
+//createElemArray();
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
+//PRACTICE #1
+
+//--------------
+
