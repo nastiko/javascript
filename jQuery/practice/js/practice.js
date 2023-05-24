6caf83098f22aacfbe504b1accd48e153a31c61b
Anastasia <anastasia.hrynkevich@outlook.com>
2023-05-24 19:56:48 +0100

    init: - practiced jQuery

---
+++ jQuery/practice/js/practice.js
@@ -0,0 +1,143 @@
+//PRACTICE #1
+function wrapElems() {
+    $('#practice1 b').parent().wrap('<div></div>');
+}
+
+//wrapElems();
+
+//--------------
+
+//PRACTICE #2
+function styleElems1() {
+    let textAddBefore = $('#practice2 .www').prepend('!');
+    textAddBefore.append('<h4>Text</h4>', '<h5>Text</h5>', '<h6>Text</h6>').css('color', 'red');
+}
+
+//styleElems1();
+
+//--------------
+
+//PRACTICE #3
+function replaceTags() {
+    $('#practice3 div p').each(function () {
+        $(this).replaceWith('<div>' + $(this).text() + '</div>');
+    });
+}
+
+replaceTags();
+
+//--------------
+
+//PRACTICE #4
+function disabledCheckbox() {
+    $('#practice4 div input:odd').prop('disabled', true);
+    $('#practice4 div input:even').prop('checked', 'checked');
+}
+
+//disabledCheckbox();
+
+//--------------
+
+//PRACTICE #5
+function addAppendPrependElem() {
+    $('#practice5 div').append('<p>#test</p>').prepend('<p>#test</p>');
+    $('#practice5 div p').parent().prepend('Parent:');
+    $('#practice5 div p').first().append('<p>test1</p>').prepend('<p>test1</p>');
+    $('#practice5 div p').last().append('<p>test2</p>').prepend('<p>test2</p>');
+}
+
+//addAppendPrependElem();
+
+//--------------
+
+//PRACTICE #6
+function addWrapElem() {
+    $('#practice6 li:nth-child(2)').addClass('www').wrapInner('<i></i>>').append('<li>Hello</li>');
+    $('#practice6 li:nth-child(10)').parent().siblings().wrap('<div class="www"></div>');
+}
+
+//addWrapElem();
+
+//--------------
+
+//PRACTICE #7
+function addIndex() {
+    $('#practice7 div p').each(function (index) {
+        $(this).append(` is index: ${index}`);
+    });
+}
+
+//addIndex();
+
+//--------------
+
+//PRACTICE #8
+function cloneElem() {
+    $('#practice8 div p').css('color', 'green');
+    $('#practice8 div p').each(function () {
+        $(this).clone().insertBefore($(this)).css('color', 'red');
+    });
+}
+
+//cloneElem();
+
+//--------------
+
+//PRACTICE #9
+function prependElems() {
+    $('#practice9 div h2').before($('#practice9 div p'));
+}
+
+//prependElems();
+
+//--------------
+
+//PRACTICE #10
+function addText() {
+    $('#practice10 div p:empty').html(`New text: ` + $('#practice10 div h2').text());
+}
+
+//addText();
+
+//--------------
+
+//PRACTICE #11
+function styleElem2() {
+    $('#practice11 i.www').css({color: 'red', fontSize: '30px'});
+}
+
+//styleElem2();
+
+//--------------
+
+//PRACTICE #12
+function countSquares() {
+
+    $('#practice12 input').each(function () {
+        $(this).val(Number($(this).val()) ** 2);
+    });
+}
+
+//countSquares();
+
+//--------------
+
+//PRACTICE #13
+function styleElem3() {
+    $('#practice13 div :not(:header)').css('color', 'red');
+    $('#practice13 div p.www').append('!!!!').css('color', 'green');
+
+}
+
+//styleElem3();
+
+//--------------
+
+//PRACTICE #23
+
+//--------------
+
+
+//PRACTICE #1
+
+//--------------
